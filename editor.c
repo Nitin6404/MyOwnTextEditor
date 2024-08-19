5d02e464cead0be960a3e579c9c3cfe5b89ff0b2
nitin6404 <nc1575612@gmail.com>
2024-08-19 19:40:06 +0530

    checkpoint: text input is working for now

--- editor.c
+++ editor.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
 
+struct termios orig_termios;
 
-int main(){
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);  // Restore original terminal settings
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);  // Save original terminal settings
+    atexit(disableRawMode);  // Register disableRawMode to be called on exit
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);  // Disable canonical mode and echoing
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);  // Apply the new settings
+}
+
+int main() {
+    enableRawMode();
+    
     char c;
-    printf("Hello there this is me, Nitin!!!!");
-    while(read(STDIN_FILENO, &c, 1) == 1);
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        printf("%c", c);
+        fflush(stdout);  // Force the output to be flushed immediately
+    }
+    
     return 0;
 }
